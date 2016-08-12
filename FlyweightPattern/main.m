ad53b3bcecaec6d45c3e4eb35f30d2aa656eb5fd
rhc <bawfnhaps@163.com>
2016-08-12 10:53:39 +0800

    Initial Commit

---
+++ FlyweightPattern/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FlyweightPattern
+//
+//  Created by hairong chen on 16/8/12.
+//  Copyright © 2016年 hairong chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
